a6a008cb6108098cc5cb654fe4809981d176171e
glomdom <sadonkovkiril@gmail.com>
2025-10-24 22:53:42 +0300

    Initial commit

---
+++ BinaryWizard/BinaryWizard/SyntaxExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace BinaryWizard;
+
+public static class SyntaxExtensions {
+    public static MethodDeclarationSyntax AddParameters(this MethodDeclarationSyntax method, params (string name, string type)[] parameters) {
+        var list = SyntaxFactory.SeparatedList(parameters.Select(p =>
+            SyntaxFactory.Parameter(SyntaxFactory.Identifier(p.name))
+                .WithType(SyntaxFactory.ParseTypeName(p.type))
+        ));
+
+        return method.WithParameterList(SyntaxFactory.ParameterList(list));
+    }
+}
