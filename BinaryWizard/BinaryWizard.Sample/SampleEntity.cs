511d09e76e21bd23229b345966b72ba7a794518a
glomdom <103685817+glomdom@users.noreply.github.com>
2025-10-24 22:53:42 +0300

    Initial commit

---
+++ BinaryWizard/BinaryWizard.Sample/SampleEntity.cs
@@ -0,0 +1,9 @@
+using BinaryWizard.Generated;
+
+namespace BinaryWizard.Sample;
+
+[BinaryStruct]
+public partial class SampleEntity {
+    public int Id;
+    public int Name;
+}
---
+++ BinaryWizard/BinaryWizard/Diagnostics.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace BinaryWizard;
+
+internal static class Diagnostics {
+    internal static readonly DiagnosticDescriptor MissingFileError = new DiagnosticDescriptor(
+        id: "BINWZ0001",
+        title: "Missing nif.xml",
+        messageFormat: "Required configuration file 'nif.xml' was not found in the project",
+        category: "BinaryWizard",
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+}
