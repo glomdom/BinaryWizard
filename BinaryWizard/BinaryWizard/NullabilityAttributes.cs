c6b5d7240a1eaa341c4ace396b9bbfe40ca6a4a4
glomdom <103685817+glomdom@users.noreply.github.com>
2025-10-27 22:29:58 +0200

    feat: `NotNullWhen` verbatim

---
+++ BinaryWizard/BinaryWizard/NullabilityAttributes.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// This is a polyfill for nullability attributes that aren't available in .NET Standard 2.0
+// Safe to include in your generator or analyzer projects.
+
+#nullable enable
+
+namespace System.Diagnostics.CodeAnalysis {
+    [AttributeUsage(AttributeTargets.Parameter)]
+    internal sealed class NotNullWhenAttribute : Attribute {
+        public NotNullWhenAttribute(bool returnValue) {
+            ReturnValue = returnValue;
+        }
+
+        public bool ReturnValue { get; }
+    }
+}
