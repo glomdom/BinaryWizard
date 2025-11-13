fabe4d0587141d6bf1f55893a3c9b85e63e5d815
glomdom <103685817+glomdom@users.noreply.github.com>
2025-11-13 17:48:36 +0200

    feat: add type information to field def

---
+++ BinaryWizard/DebugUtilities.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using BinaryWizard.Model;
+
+namespace BinaryWizard;
+
+public static class DebugUtilities {
+    [Conditional("DEBUG")]
+    public static void CreatedFieldDef(FieldDef def) {
+        Debug.WriteLine($"Created field definition {def.Name} of {def.TypeModel.Type} ({def.ByteSize} bytes) (dynamic? {def.IsDynamic})");
+    }
+}
