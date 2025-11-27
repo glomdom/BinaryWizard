0f42398023ce1c6e4c44e6264065e913a3fc854a
glomdom <103685817+glomdom@users.noreply.github.com>
2025-11-27 17:09:55 +0200

    feat(test): test dynamics /doesnt work/

--- BinaryWizard.Tests/Samples/Arrays.cs
+++ BinaryWizard.Tests/Samples/Arrays.cs
@@ -2,6 +2,9 @@ namespace BinaryWizard.Tests.Samples;
 
 [BinarySerializable]
 public partial struct Arrays {
-    [BinaryArray(Size = 16)]
-    public int[] NumberArray;
+    [BinaryArray(Size = 16)] public int[] NumberArray;
+    public int OtherNumbersSize;
+
+    [BinaryArray(SizeMember = nameof(OtherNumbersSize))]
+    public int[] OtherNumbers;
 }
--- BinaryWizard.Tests/SerializerTests.cs
+++ BinaryWizard.Tests/SerializerTests.cs
@@ -48,6 +48,12 @@ public class SerializerTests {
             for (var i = 0; i < 16; i++) {
                 writer.Write(i);
             }
+            
+            writer.Write(4);
+            
+            for (var i = 0; i < 4; i++) {
+                writer.Write(i);
+            }
         }
 
         stream.Position = 0;
@@ -56,9 +62,13 @@ public class SerializerTests {
         var actual = Arrays.FromBinary(reader);
         var expected = new Arrays {
             NumberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            OtherNumbersSize = 4,
+            OtherNumbers = [0, 1, 2, 3],
         };
 
         Assert.Equal(expected.NumberArray, actual.NumberArray);
+        Assert.Equal(expected.OtherNumbersSize, actual.OtherNumbersSize);
+        Assert.Equal(expected.OtherNumbers, actual.OtherNumbers);
     }
 
     // [Fact]
